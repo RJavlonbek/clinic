d25893184b3a50125787bddf65f344e454f97dc6
RJavlonbek <javlonarjei042@gmail.com>
2021-07-15 11:38:15 +0500

    update password bug fix

--- models/change_item.php
+++ models/change_item.php
@@ -81,14 +81,16 @@ if(isset($_POST['item'])){
 		if(isset($_POST['old_password'])){
 			if(isset($_POST['role'])){
 				$role=$db->quote($_POST['role']);
-				$id = $db->quote($_POST['id']);
+				$id = $_POST['id'] ? $db->quote($_POST['id']) : "";
 				$old_password=$db->quote($_POST['old_password']);
 				$new_password=$db->quote($_POST['new_password']);
 
 				$sql="SELECT parol FROM xodimlar WHERE rol=$role";
+				
 				if($id) {
 					$sql = $sql . " AND id=$id";
 				}
+				
 				$password=$db->query($sql);
 				$password=$password->fetchAll(PDO::FETCH_ASSOC);
 				$status=0;
